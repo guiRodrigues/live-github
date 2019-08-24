85bd9cf0fb7589aa44d26c6b73cb4a3fd3d0dfaa
Guilherme <guilherme.imlerith@gmail.com>
2019-08-24 13:48:01 -0300

    chore: Altera o retorno da rota

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Live sobre git" });
+  return res.json({ message: "Live sobre git..." });
 });
 
 app.listen(3333);
